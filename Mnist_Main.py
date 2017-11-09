eebca8eb78f489289fe3b8b4d9cdad14ad877cd5
blakecalvin <blakecalvin@gmail.com>
2017-11-09 12:29:51 -0800

    removed SOM and made Outliers method

--- Mnist_Main.py
+++ Mnist_Main.py
@@ -1,13 +1,13 @@
 import Mnist
 
-'''
-    arg1 = model
-        1 = KNN
-        2 = SOM
-    arg2 = Distance algorithm
-        1 = Manhattan
-        2 = Euclidian
-        3 = Minkowski (p=3)
-    arg3 = k
-'''
-Mnist.main(arg1=2,arg3=5)
+    #    arg1 = model
+    #        1 = KNN
+    #        2 = Outliers
+    #    arg2 = Distance algorithm
+    #        1 = Manhattan
+    #        2 = Euclidian
+    #        3 = Minkowski (p=3)
+    #    arg3 = k
+    #    arg4 = k_means
+
+Mnist.main(arg1=1,arg3=5)
